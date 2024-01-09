2fdade62e6d8c23e798f22f9587dec97e14d7497
Romel Meza <53792824+Ramm23@users.noreply.github.com>
2024-01-09 11:02:09 +0100

    Add files via upload
    Initial file for the project

---
+++ Mailbox/Mailbox.ino
@@ -0,0 +1,17 @@
+/*
+
+Crerated by Romel Meza,
+on 09/01/2024
+
+*/
+
+#define N 0;
+//macros
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
