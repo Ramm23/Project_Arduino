8a487b942a38519ac440745cdca25f9809fd268e
Ramm23 <53792824+Ramm23@users.noreply.github.com>
2024-01-09 16:10:23 +0100

    RFID Constants
    added pins for RFID

--- Mailbox/Mailbox.ino
+++ Mailbox/Mailbox.ino
@@ -11,6 +11,11 @@ on 09/01/2024
 
 // Macros
 #define N 0;
+#include <SPI.h>
+#include <MFRC522.h>
+#define RST_PIN 9                 // Configurable, see typical pin layout above
+#define SS_PIN 10                 // Configurable, see typical pin layout above
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 
 // Global Variables
 
@@ -18,6 +23,7 @@ const char Servo_motor = D1;
 
 void setup()
 {
+  Serial.begin(115200);
   // put your setup code here, to run once:
 }
 
