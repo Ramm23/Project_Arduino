3da58af291069734569a2aa224d377b940aa41d7
Ramm23 <53792824+Ramm23@users.noreply.github.com>
2024-01-09 11:15:56 +0100

    Added Template code

--- Mailbox/Mailbox.ino
+++ Mailbox/Mailbox.ino
@@ -5,13 +5,23 @@ on 09/01/2024
 
 */
 
-#define N 0;
-//macros
+// Libraries
+#include <ESP8266WiFi.h>
+#include <Servo.h>
 
-void setup() {
+// Macros
+#define N 0;
+
+// Global Variables
+
+const char Servo_motor = D1;
+
+void setup()
+{
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
