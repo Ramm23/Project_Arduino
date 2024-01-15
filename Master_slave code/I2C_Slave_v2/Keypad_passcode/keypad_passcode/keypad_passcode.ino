998a2e0be5dd9007256fe4af81011dd6d520ced4
maxbrazhnyy <mbv508@gmail.com>
2024-01-15 18:36:04 +0100

    reading 4 digit passcodes in keypad_passcode.ino

--- Master_slave code/I2C_Slave_v2/Keypad_passcode/keypad_passcode/keypad_passcode.ino
+++ Master_slave code/I2C_Slave_v2/Keypad_passcode/keypad_passcode/keypad_passcode.ino
@@ -9,19 +9,19 @@
 */
 #include <Keypad.h>
 
-const byte ROWS = 4; //four rows
-const byte COLS = 4; //four columns
+const byte ROWS = 4;  //four rows
+const byte COLS = 4;  //four columns
 char keys[ROWS][COLS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
 };
 
-byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {9, 8, 7, 6}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = { 5, 4, 3, 2 };  //connect to the row pinouts of the keypad
+byte colPins[COLS] = { 9, 8, 7, 6 };  //connect to the column pinouts of the keypad
 
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 char* passcodeA;
 char* passcodeB;
@@ -30,48 +30,52 @@ char* passcodeD;
 char* passcode;
 char key;
 int i = 0;
+String inputString;
+long inputInt;
 
-void setup(){
+void setup() {
   Serial.begin(115200);
   passcodeA = "1234";
   passcodeA = "2345";
   passcodeA = "3456";
   passcodeA = "4567";
-
 }
-  
-void loop(){
+
+void loop() {
   //run_keypad();
-  i = 0;
-  char full_passcode = register_key();
-  Serial.print("\nTotal Received Passcode: ");
-  Serial.print(full_passcode);
-  delay(10*1000);
+  // i = 0;
+  //char full_passcode = register_key();
+  check_code();
+  // Serial.print("\nTotal Received Passcode: ");
+  // Serial.print(full_passcode);
+  Serial.print("Performing Delay of 10 seconds");
+  delay(10 * 1000);
 }
 
-void run_keypad(){
+void run_keypad() {
   Serial.print("\nInput passcode on numpad");
-  
-  while (key == NO_KEY){
-      key = keypad.getKey();
-  }  
-  for (int i =0; i<3 ; i++){
+
+  while (key == NO_KEY) {
+    key = keypad.getKey();
+  }
+  for (int i = 0; i < 3; i++) {
     //while (key == NO_KEY){
     //key = keypad.getKey();
     passcode[i] = key;
     Serial.print("\nGot key: ");
     Serial.print(key);
     key = NO_KEY;
-    while (key == NO_KEY){
+    while (key == NO_KEY) {
       key = keypad.getKey();
     }
   }
   Serial.print("\nTotal Received Passcode: ");
   Serial.print(passcode);
-
 }
-void check_passcode(){
-  if (passcode == passcodeA){
+
+
+void check_passcode() {
+  if (passcode == passcodeA) {
     Serial.print("\npasscode == passcodeA");
   }
 }
@@ -96,17 +100,50 @@ char register_key(){
 
 */
 
-char register_key() {
+/*char register_key() {
   Serial.print("\nInput passcode on numpad");
-  while(i<4){
+  for(int i = 0; i < 4; i++){
+    while(key == NO_KEY){
+      //Serial.print("\nWaiting for key inside while loop\n");
+      key = keypad.getKey();
+    }
+    passcode[i] = key;
+    Serial.print("\nGot key: ");
+    Serial.print(key);
     key = keypad.getKey();
-    if (key != NO_KEY)
-      i = i+1;
-      passcode[i] = key;
-      Serial.print("\nGot key: ");
-      Serial.print(key);
   }
   return passcode;
 }
+*/
 
 
+void check_code() {  //code from https://arduinogetstarted.com/faq/how-to-input-a-multiple-digits-number-using-the-keypad?utm_content=cmp-true
+  inputString = "";  // Reset the inputString for the next input
+  while (inputString.length() != 4) {
+
+    char key = keypad.getKey();
+    while (key == NO_KEY) {
+      key = keypad.getKey();
+    }
+    if (key) {
+      Serial.println(key);
+
+      if (key >= '0' && key <= '9') {  // only act on numeric keys
+        inputString += key;            // append new character to input string
+
+
+      } else if (key == '#') {
+        if (inputString.length() > 0) {
+          inputInt = inputString.toInt();  // YOU GOT AN INTEGER NUMBER
+          inputString = "";                // clear input
+          // DO YOUR WORK HERE
+        }
+      } else if (key == '*') {
+        inputString = "";  // clear input
+      }
+    }
+  }
+  Serial.print("Full passcode (inputString): ");
+  Serial.println(inputString);
+  return;
+}
